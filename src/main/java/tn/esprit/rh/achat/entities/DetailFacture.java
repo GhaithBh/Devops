7035d2c1ec596bcdc70f4bd9ca175bcfd74042b9
fatimetouu19 <fatimetou.cheikhneda@esprit.tn>
2022-11-09 18:19:44 +0000

    update pom.xml

--- src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
+++ src/main/java/tn/esprit/rh/achat/entities/DetailFacture.java
@@ -7,16 +7,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class DetailFacture implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -28,6 +21,62 @@ public class DetailFacture implements Serializable {
 	private float montantRemise;
 	@ManyToOne
 	private Produit produit;
+	public DetailFacture(Long idDetailFacture, Integer qteCommandee, float prixTotalDetail, Integer pourcentageRemise,
+			float montantRemise, Produit produit, Facture facture) {
+		super();
+		this.idDetailFacture = idDetailFacture;
+		this.qteCommandee = qteCommandee;
+		this.prixTotalDetail = prixTotalDetail;
+		this.pourcentageRemise = pourcentageRemise;
+		this.montantRemise = montantRemise;
+		this.produit = produit;
+		this.facture = facture;
+	}
+	public Long getIdDetailFacture() {
+		return idDetailFacture;
+	}
+	public void setIdDetailFacture(Long idDetailFacture) {
+		this.idDetailFacture = idDetailFacture;
+	}
+	public Integer getQteCommandee() {
+		return qteCommandee;
+	}
+	public void setQteCommandee(Integer qteCommandee) {
+		this.qteCommandee = qteCommandee;
+	}
+	public float getPrixTotalDetail() {
+		return prixTotalDetail;
+	}
+	public void setPrixTotalDetail(float prixTotalDetail) {
+		this.prixTotalDetail = prixTotalDetail;
+	}
+	public Integer getPourcentageRemise() {
+		return pourcentageRemise;
+	}
+	public void setPourcentageRemise(Integer pourcentageRemise) {
+		this.pourcentageRemise = pourcentageRemise;
+	}
+	public float getMontantRemise() {
+		return montantRemise;
+	}
+	public void setMontantRemise(float montantRemise) {
+		this.montantRemise = montantRemise;
+	}
+	public Produit getProduit() {
+		return produit;
+	}
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+	public Facture getFacture() {
+		return facture;
+	}
+	public void setFacture(Facture facture) {
+		this.facture = facture;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	@ManyToOne
 	@JsonIgnore
 	Facture facture;
